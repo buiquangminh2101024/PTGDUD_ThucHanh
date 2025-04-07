03d28189b0954a712151c6667b6aad680335ffa6
buiquangminh2101024 <buiquangminh91otcc@gmail.com>
2025-04-07 18:27:36 +0700

    Lab7 Add user Modal API post

--- Lab7/server/server.js
+++ Lab7/server/server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import cors from "cors"
+import path from 'path'
+import fs from 'fs'
+import multer from 'multer'
+import { fileURLToPath } from "url";
+
 const app = express();
 const port = 8000;
+const __filename = fileURLToPath(import.meta.url);//giúp tạo đường dẫn đúng
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "../src/assets");
 
 app.use(cors())
 
@@ -11,106 +19,81 @@ app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
-var data = [
-    {
-        id: 1,
-        title: 'Turnover',
-        amount: 92405,
-        periodOfChange: 5.39,
-        backgroundColor: 'bg-pink-200'
-    },
-    {
-        id: 2,
-        title: 'Profit',
-        amount: 32218,
-        periodOfChange: 5.39,
-        backgroundColor: 'bg-purple-200'
-    },
-    {
-        id: 3,
-        title: 'New customer',
-        amount: 298,
-        periodOfChange: 6.84,
-        backgroundColor: 'bg-blue-200'
-    },
-]
+////////////
+//Get data//
+////////////
+var rawData = fs.readFileSync(path.join(filePath, 'data.txt'), "utf8");
+var data = JSON.parse(rawData)
 
 app.get("/overview", (req, res) => {
     res.json(data);
 });
 
-var reportData = [
-    {
-        id: 1,
-        customerName: "Elizabeth Lee",
-        company: "AvatarSystems",
-        orderValue: 359,
-        orderDate: new Date(2023, 6, 10).toISOString(),
-        status: "New",
-        img: 0
-    },
-    {
-        id: 2,
-        customerName: "Carlos Garcia",
-        company: "SmoozeShift",
-        orderValue: 747,
-        orderDate: new Date(2023, 6, 24).toISOString(),
-        status: "New",
-        img: 1
-    },
-    {
-        id: 3,
-        customerName: "Elizabeth Balley",
-        company: "Prime Time Telecom",
-        orderValue: 564,
-        orderDate: new Date(2023, 7, 8).toISOString(),
-        status: "In-progress",
-        img: 2
-    },
-    {
-        id: 4,
-        customerName: "Ryan Brown",
-        company: "OmniTeach Corporation",
-        orderValue: 541,
-        orderDate: new Date(2023, 7, 31).toISOString(),
-        status: "In-progress",
-        img: 3
-    },
-    {
-        id: 5,
-        customerName: "Ryan Young",
-        company: "DataStram Inc.",
-        orderValue: 769,
-        orderDate: new Date(2023, 4, 1).toISOString(),
-        status: "Completed",
-        img: 4
-    },
-    {
-        id: 6,
-        customerName: "Hailey Adams",
-        company: "FlowRush",
-        orderValue: 922,
-        orderDate: new Date(2023, 5, 10).toISOString(),
-        status: "Completed",
-        img: 5
-    }
-]
+//////////////////
+//Get reportData//
+//////////////////
+var reportData
+try {
+    const data = fs.readFileSync(path.join(filePath, "reportData.txt"), "utf8");
+    const jsonData = JSON.parse(data);
+    jsonData.orderDate = new Date(jsonData.orderDate);
+
+    reportData = jsonData; // Gán giá trị cho biến
+} catch (err) {
+    console.error("Lỗi:", err.message);
+}
 
 app.get("/report", (req, res) => {
     res.json(reportData)
 })
 
+/////////////
+//Get image//
+/////////////
+app.use("/report/images",express.static(filePath));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(filePath, "/cachedImg"));
+    },
+    filename: (req, file, cb) => {
+        const fileName = req.body.img.split('/')
+        cb(null, fileName[fileName.length - 1]);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+/////////////
+//Edit user//
+/////////////
 var lastUserEdited
 
-app.put("/editUser", (req, res) => {
+app.put("/editUser", upload.single("imgFile"), (req, res) => {
     console.log('BODY RECEIVED:', req.body)
     lastUserEdited = req.body
-    // reportData = reportData.map(user => user.id == req.body.id ? { ...user, ...req.body } : user)
     const index = reportData.findIndex(user => user.id == req.body.id)
-      reportData[index] = {...reportData[index], ...req.body}
+    reportData[index] = { ...reportData[index], ...req.body }
     res.json({ message: 'Ok' })
 })
 
 app.get("/editUser/lastEdit", (req, res) => {
     res.json(lastUserEdited)
+})
+
+/////////////
+//Add user//
+/////////////
+var lastUserAdded
+
+app.post("/addUser", upload.single("imgFile"), (req, res) => {
+    console.log('BODY RECEIVED:', req.body)
+    const newUser = {id: reportData.length + 1, ...req.body}
+    lastUserAdded = newUser
+    reportData.push(newUser)
+    res.json({ message: 'Ok' })
+})
+
+app.get("/addUser/lastAdd", (req, res) => {
+    res.json(lastUserAdded)
 })
